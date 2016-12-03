a4a3532f3192169406a3b9d585361c4575a7469c
Lomdjaria <serega_7796@mail.ru>
2016-12-04 02:40:06 +0300

    1-st Final. All tests were passed;

--- src/com/company/homework3/ArrayStack.java
+++ src/com/company/homework3/ArrayStack.java
@@ -4,57 +4,67 @@ package com.company.homework3;
  * Created by Lomdji on 03.12.2016.
  */
 import java.util.Iterator;
-
+import java.util.EmptyStackException;
 import static java.lang.System.arraycopy;
 
 public class ArrayStack<T> implements Stack<T> {
     private Object[] array;
-    private int size=0;
+    private long size=0;
 
-    public ArrayStack(int capacity) {
-        array=new Object[capacity];
-    }
-
-    // Constructor by Base
-    public ArrayStack(T[] Base) {
-        array=Base.clone();
-        size=Base.length;
+    public ArrayStack(){
+        array=new Object[2];
     }
 
     @Override
     public T push(T item) {
-        throw new UnsupportedOperationException();
+        if(size==array.length) {
+            Object[] tmp = new Object[(int) size*2];
+            arraycopy(array, 0, tmp, 0, (int) size);
+            array = tmp;
+        }
+        array[(int)size++]=item;
+        return item;
     }
 
     @Override
     public T pop() {
-        throw new UnsupportedOperationException();
+        if(size != 0)
+            return (T)array[(int)--size];
+        else
+            throw new EmptyStackException();
+
     }
 
     @Override
     public T peek() {
-        throw new UnsupportedOperationException();
+        if(size != 0)
+            return (T)array[(int)(size-1)];
+        else
+            throw new EmptyStackException();
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+        return size==0?true:false;
     }
 
     @Override
     public long getSize() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public Iterator<T> iterator() {
-        throw new UnsupportedOperationException();
-    }
+        return new Iterator<T>() {
+            long current = 0;
+            public boolean hasNext() {
+                return (current < size);
+            }
 
-    private void enlarge(int new_size) {
-        Object[] tmp=new Object[new_size];
-        arraycopy(array,0,tmp,0,array.length);
-        array=tmp;
+            @Override
+            public T next() {
+                return (T)array[(int)current++];
+            }
+        };
     }
-
 }
